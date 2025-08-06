88f5cd19a14633d17f591e4527425584a69f79ee
suyeon <paksy0428@gmail.com>
2025-08-06 16:24:33 +0900

    feat: 참여한 방 목록 페이지네이션 구현

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,18 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import CodeTogetherLogo from '../assets/code_together_logo.png';
-import {
-  ListBulletIcon,
-  UserIcon,
-  CodeBracketIcon,
-  CalendarIcon,
-  ArrowRightIcon,
-} from '@heroicons/react/24/solid';
+import { ListBulletIcon } from '@heroicons/react/24/solid';
 import LogModal from '../components/mypage/LogModal';
 import Button from '../components/common/Button';
 import { useNavigate } from 'react-router-dom';
 import { logout } from '../utils/logout';
-import { useUserStore } from '../stores/useUserStore'; // Zustand store
+import { useUserStore } from '../stores/useUserStore';
 import api from '../utils/api';
+import RoomCard from '../components/mypage/RoomCard';
+import Pagination from '../components/mypage/Pagination';
 
 interface Room {
   id: number;
@@ -25,14 +21,21 @@ interface Room {
 
 const MyPage = () => {
   const [rooms, setRooms] = useState<Room[]>([]);
-  const [selectedRoom, setSelectedRoom] = useState<Room | null>(null); // 모달 상태
-
+  const [selectedRoom, setSelectedRoom] = useState<Room | null>(null);
   const navigate = useNavigate();
   const { user } = useUserStore();
 
   const handleLogout = () => {
     logout(navigate);
   };
+
+  // Pagination
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
+  const indexOfLast = currentPage * itemsPerPage;
+  const indexOfFirst = indexOfLast - itemsPerPage;
+  const currentRooms = rooms.slice(indexOfFirst, indexOfLast);
+  const totalPages = Math.ceil(rooms.length / itemsPerPage);
 
   useEffect(() => {
     const fetchRooms = async () => {
@@ -73,59 +76,30 @@ const MyPage = () => {
         </div>
 
         {/* 참여한 방 목록 */}
-        <div className="bg-white border border-gray-200 rounded-sm p-6">
+        <div className="bg-white border border-gray-200 rounded-sm p-6 relative h-[570px]">
           <div className="flex gap-2 mb-6">
             <ListBulletIcon className="w-6 h-6 text-gray-800 mt-0.5" />
             <p className="text-lg font-bold text-gray-800">참여했던 방 목록</p>
           </div>
+
           <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-2">
-            {rooms.map((room) => (
-              <div
+            {currentRooms.map((room) => (
+              <RoomCard
                 key={room.id}
-                className="bg-white border border-primary-100 p-6 hover:shadow-md transition-shadow"
-              >
-                <h4 className="font-semibold text-gray-800 mb-4">
-                  {room.title}
-                </h4>
-                <div className="flex gap-2 mb-1">
-                  <CalendarIcon className="w-4 h-4 text-gray-800 mt-0.5" />
-                  <p className="text-sm text-gray-500">
-                    {new Date(room.createdAt).toLocaleDateString()}
-                  </p>
-                </div>
-                <div className="flex gap-2 mb-1">
-                  <CodeBracketIcon className="w-4 h-4 text-gray-800 mt-0.5" />
-                  <p className="text-sm text-gray-500">{room.language}</p>
-                </div>
-                <div className="flex gap-2 mb-1">
-                  <UserIcon className="w-4 h-4 text-gray-800 mt-0.5" />
-                  <p className="text-sm text-gray-500">{room.ownerNickname}</p>
-                </div>
-                <div className="flex justify-between items-center mt-4">
-                  <div
-                    className={`text-xs font-bold rounded-full h-6 flex items-center justify-center ${
-                      room.mode === '문제풀이'
-                        ? 'text-green-600 bg-green-100 w-19'
-                        : 'text-primary-600 bg-primary-100 w-16'
-                    }`}
-                  >
-                    {room.mode}
-                  </div>
-                  <button
-                    className="flex gap-1 mb-1 text-blue-600 cursor-pointer"
-                    onClick={() => setSelectedRoom(room)}
-                  >
-                    <p className="text-xs font-medium hover:font-bold">
-                      로그 보기
-                    </p>
-                    <ArrowRightIcon className="w-4 h-4" />
-                  </button>
-                </div>
-              </div>
+                room={room}
+                onClick={() => setSelectedRoom(room)}
+              />
             ))}
           </div>
+
+          <Pagination
+            totalPages={totalPages}
+            currentPage={currentPage}
+            onPageChange={setCurrentPage}
+          />
         </div>
       </div>
+
       {selectedRoom && (
         <LogModal room={selectedRoom} onClose={() => setSelectedRoom(null)} />
       )}
