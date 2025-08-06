cb09080f059de24f69e4360d020dc7ba585a10b3
Kang Ye Ram <kyj5730@gmail.com>
2025-08-06 14:01:04 +0900

    rename: session.ts파일을 /services 폴더로 옮김

--- src/services/session.ts
+++ src/services/session.ts
@@ -1,5 +1,28 @@
 import axios from 'axios';
 
+export interface Participant {
+  id: number;
+  nickname: string;
+}
+
+export interface SessionDetail {
+  id: number;
+  title: string;
+  mode: string;
+  language: string;
+  ownerId: number;
+  alreadyJoined: boolean;
+  participants: Participant[];
+}
+
+export interface Session {
+  id: number;
+  title: string;
+  language: string;
+  mode: '문제풀이' | '웹편집';
+  isEnded: boolean;
+}
+
 export const createSession = async (
   token: string,
   data: { title: string; mode: string; language: string },
@@ -16,7 +39,10 @@ export const createSession = async (
   return res.data;
 };
 
-export const getSessionList = async (page = 1, limit = 20) => {
+export const getSessionList = async (
+  page = 1,
+  limit = 20,
+): Promise<Session[]> => {
   const res = await axios.get(
     `${import.meta.env.VITE_API_BASE_URL}/sessions?page=${page}&limit=${limit}`,
   );
@@ -24,7 +50,10 @@ export const getSessionList = async (page = 1, limit = 20) => {
   return res.data.sessions;
 };
 
-export const joinSession = async (token: string, sessionId: number) => {
+export const joinSession = async (
+  token: string,
+  sessionId: number,
+): Promise<SessionDetail> => {
   const res = await axios.post(
     `${import.meta.env.VITE_API_BASE_URL}/sessions/${sessionId}/join`,
     {},
