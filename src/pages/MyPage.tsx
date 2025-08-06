6a6240ca9782bc944e695150cc29d4b635dfcdac
FF <wjdrldud98@gmail.com>
2025-08-06 15:02:30 +0900

    fix: eslint error 수정 - import React 제거

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import CodeTogetherLogo from '../assets/code_together_logo.png';
 import {
   ListBulletIcon,
