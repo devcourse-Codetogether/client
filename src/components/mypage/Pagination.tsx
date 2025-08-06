5e6463a4e8dc73b00b8ffb525c98a5f3ea41a128
suyeon <paksy0428@gmail.com>
2025-08-06 16:26:13 +0900

    design: 디자인 수정

--- src/components/mypage/Pagination.tsx
+++ src/components/mypage/Pagination.tsx
@@ -23,9 +23,9 @@ const Pagination: React.FC<PaginationProps> = ({
           <button
             key={page}
             onClick={() => onPageChange(page)}
-            className={`w-8 h-8 rounded-full text-sm flex items-center justify-center border transition ${
+            className={`w-8 h-8 rounded-full text-sm flex items-center justify-center border transition cursor-pointer ${
               isActive
-                ? 'bg-blue-500 text-white border-blue-500'
+                ? 'bg-primary-500 text-white border-blue-500'
                 : 'bg-white text-gray-700 border-gray-300 hover:bg-gray-100'
             }`}
           >
