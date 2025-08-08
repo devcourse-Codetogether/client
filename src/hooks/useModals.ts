3f769253a170f08e5c0d53fbbdb10fc96ef4dcf1
Kang Ye Ram <kyj5730@gmail.com>
2025-08-08 16:00:43 +0900

    fix: 필터 및 검색기능 삭제

--- src/hooks/useModals.ts
+++ src/hooks/useModals.ts
@@ -2,14 +2,10 @@ import { useState } from 'react';
 
 export const useModals = () => {
   const [isCreateRoomModalOpen, setIsCreateRoomModalOpen] = useState(false);
-  const [isFilterModalOpen, setIsFilterModalOpen] = useState(false);
 
   return {
     isCreateRoomModalOpen,
-    isFilterModalOpen,
     openCreateRoomModal: () => setIsCreateRoomModalOpen(true),
     closeCreateRoomModal: () => setIsCreateRoomModalOpen(false),
-    openFilterModal: () => setIsFilterModalOpen(true),
-    closeFilterModal: () => setIsFilterModalOpen(false),
   };
 };
